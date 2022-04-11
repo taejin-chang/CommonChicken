8816838e8ef2709d0ae417e5e731dab85d82b4a2
jmaster1020 <jmaster1020@gmail.com>
2022-04-11 12:50:46 +0900

    add: replyVO

---
+++ src/main/java/com/commonchicken/dto/ReplyVO.java
@@ -0,0 +1,148 @@
+package com.commonchicken.dto;
+
+public class ReplyVO {
+	private int revNum;
+	private String memEmail2;
+	private String ordDetailNum;
+	private int cmNum;
+	private String revContent;
+	private int revRated;
+	private String revDate;
+	private String revUpload;
+	private String prdName;
+	private String stoNum;
+	private int rplNum;
+	private String memEmail;
+	private String rplContent;
+	private String rplDate;
+	private String stoName;
+	
+	public String getStoName() {
+		return stoName;
+	}
+
+	public void setStoName(String stoName) {
+		this.stoName = stoName;
+	}
+
+	public ReplyVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRevNum() {
+		return revNum;
+	}
+
+	public void setRevNum(int revNum) {
+		this.revNum = revNum;
+	}
+
+	public String getMemEmail2() {
+		return memEmail2;
+	}
+
+	public void setMemEmail2(String memEmail2) {
+		this.memEmail2 = memEmail2;
+	}
+
+	public String getOrdDetailNum() {
+		return ordDetailNum;
+	}
+
+	public void setOrdDetailNum(String ordDetailNum) {
+		this.ordDetailNum = ordDetailNum;
+	}
+
+	public int getCmNum() {
+		return cmNum;
+	}
+
+	public void setCmNum(int cmNum) {
+		this.cmNum = cmNum;
+	}
+
+	public String getRevContent() {
+		return revContent;
+	}
+
+	public void setRevContent(String revContent) {
+		this.revContent = revContent;
+	}
+
+	public int getRevRated() {
+		return revRated;
+	}
+
+	public void setRevRated(int revRated) {
+		this.revRated = revRated;
+	}
+
+	public String getRevDate() {
+		return revDate;
+	}
+
+	public void setRevDate(String revDate) {
+		this.revDate = revDate;
+	}
+
+	public String getRevUpload() {
+		return revUpload;
+	}
+
+	public void setRevUpload(String revUpload) {
+		this.revUpload = revUpload;
+	}
+
+	public String getPrdName() {
+		return prdName;
+	}
+
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+
+	public String getStoNum() {
+		return stoNum;
+	}
+
+	public void setStoNum(String stoNum) {
+		this.stoNum = stoNum;
+	}
+
+	public int getRplNum() {
+		return rplNum;
+	}
+
+	public void setRplNum(int rplNum) {
+		this.rplNum = rplNum;
+	}
+
+	public String getMemEmail() {
+		return memEmail;
+	}
+
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+
+	public String getRplContent() {
+		return rplContent;
+	}
+
+	public void setRplContent(String rplContent) {
+		this.rplContent = rplContent;
+	}
+
+	public String getRplDate() {
+		return rplDate;
+	}
+
+	public void setRplDate(String rplDate) {
+		this.rplDate = rplDate;
+	}
+
+	
+	
+
+}
+
